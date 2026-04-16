d94235221d79098f4cd1420e9d934f4b708c0918
PrishaMaiti <prisha.maiti@gmail.com>
2026-04-15 23:57:40 -0700

    Implement navigation and theme functionality across all pages; add global JavaScript for dynamic navigation and theme selection.

---
+++ global.js
@@ -0,0 +1,95 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+let pages = [
+  { url: '', title: 'Home' },
+  { url: 'projects/', title: 'Projects' },
+  { url: 'contact/', title: 'Contact' },
+  { url: 'resume/', title: 'Resume' },
+  { url: 'https://github.com/PrishaMaiti', title: 'GitHub' },
+];
+
+const BASE_PATH =
+  location.hostname === 'localhost' || location.hostname === '127.0.0.1'
+    ? '/'
+    : '/prishamaiti/';
+
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+
+for (let p of pages) {
+  let url = p.url;
+  let title = p.title;
+
+  url = !url.startsWith('http') ? BASE_PATH + url : url;
+
+  let a = document.createElement('a');
+  a.href = url;
+  a.textContent = title;
+
+  a.classList.toggle(
+    'current',
+    a.host === location.host && a.pathname === location.pathname,
+  );
+
+  if (a.host !== location.host) {
+    a.target = '_blank';
+    a.rel = 'noopener noreferrer';
+  }
+
+  nav.append(a);
+}
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+    <label class="color-scheme">
+      Theme:
+      <select>
+        <option value="light dark">Automatic</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+    </label>
+  `,
+);
+
+let select = document.querySelector('.color-scheme select');
+
+function applyTheme(scheme) {
+  document.documentElement.style.setProperty('color-scheme', scheme);
+  document.documentElement.dataset.theme =
+    scheme === 'light dark' ? 'auto' : scheme;
+}
+
+if ('colorScheme' in localStorage) {
+  applyTheme(localStorage.colorScheme);
+  select.value = localStorage.colorScheme;
+} else {
+  applyTheme(select.value);
+}
+
+select.addEventListener('input', function (event) {
+  let scheme = event.target.value;
+  applyTheme(scheme);
+  localStorage.colorScheme = scheme;
+});
+
+let form = document.querySelector('form[action^="mailto:"]');
+
+form?.addEventListener('submit', function (event) {
+  event.preventDefault();
+
+  let data = new FormData(form);
+  let params = [];
+
+  for (let [name, value] of data) {
+    params.push(`${encodeURIComponent(name)}=${encodeURIComponent(value)}`);
+  }
+
+  let url = `${form.action}?${params.join('&')}`;
+  location.href = url;
+});
