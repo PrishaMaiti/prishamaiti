9036bfe7fd30aae6e471e97bfc5c405b283da4a2
PrishaMaiti <prisha.maiti@gmail.com>
2026-04-23 22:54:29 -0700

    loaded data from API

--- global.js
+++ global.js
@@ -111,6 +111,10 @@ export async function fetchJSON(url) {
   }
 }
 
+export async function fetchGitHubData(username) {
+  return fetchJSON(`https://api.github.com/users/${username}`);
+}
+
 export function renderProjects(projects, containerElement, headingLevel = 'h2') {
   if (!(containerElement instanceof Element)) {
     console.error('renderProjects: containerElement is invalid.');
--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-import { fetchJSON, renderProjects } from './global.js';
+import { fetchJSON, fetchGitHubData, renderProjects } from './global.js';
 
 const projects = await fetchJSON('./lib/projects.json');
 const latestProjects = (projects ?? []).slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
+const githubData = await fetchGitHubData('PrishaMaiti');
+const profileStats = document.querySelector('#profile-stats');
 
 renderProjects(latestProjects, projectsContainer, 'h2');
+
+if (profileStats && githubData) {
+	profileStats.innerHTML = `
+				<dl>
+					<dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
+					<dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
+					<dt>Followers:</dt><dd>${githubData.followers}</dd>
+					<dt>Following:</dt><dd>${githubData.following}</dd>
+				</dl>
+		`;
+}
