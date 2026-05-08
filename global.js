2359180ce9ce173869667f5b87917c7a53219ea6
PrishaMaiti <prisha.maiti@gmail.com>
2026-05-08 11:06:28 -0700

    Add meta-analysis page, code analysis setup, and .gitignore

--- global.js
+++ global.js
@@ -9,6 +9,7 @@ let pages = [
   { url: 'projects/', title: 'Projects' },
   { url: 'contact/', title: 'Contact' },
   { url: 'resume/', title: 'Resume' },
+  { url: 'meta/', title: 'Meta-Analysis' },
   { url: 'https://github.com/PrishaMaiti', title: 'GitHub' },
 ];
 
